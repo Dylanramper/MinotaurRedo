7d3cbba83b3f4638e4505078ce649a4c80d3a7cc
Dylanramper <44001627+Dylanramper@users.noreply.github.com>
2020-02-12 22:51:28 -0500

    moving background

---
+++ MinotaurRedo/Assets/Scripts/backgroundMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundMovement : MonoBehaviour
+{
+    //[SerializeField] GameObject backGround;
+    [SerializeField] private float moveSpeed = 0f;
+    private float posX = 18.44f;
+    private float bound = -19.96f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = this.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveBackground();
+        
+        
+    }
+
+    void moveBackground()
+    {
+        transform.Translate(moveSpeed, 0, 0);
+        if(transform.position.x <= bound)
+        {
+            transform.position = (new Vector2(posX, 0));
+        }
+    }
+    
+}
